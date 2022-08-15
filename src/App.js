92192f9c02997e3a4531eef968d71d5cd4637bf8
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-08-16 04:29:45 +0530

    chors: Added asset loader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import logo from './jedi-logo.svg';
 const  App = ({name}) => {
-    return `<h2>welcome ${ name }</h2>`;
+    return `<h2>welcome ${ name }</h2> <img src="${ logo }" alt="jedi logo"/> `;
     
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ const webpackConfig = {
         test:/\.css$/,
         use: ["style-loader","css-loader"],
       },
+      {
+        test:/\.(png|svg|jpg|jpeg|gif)$/i,
+        type:'asset',
+      },
     ],
   },
   plugins:[
