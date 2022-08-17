4ab6860041e9939666a286fbf70694b35f2f38cb
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-08-17 09:54:14 +0530

    style: added css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import _ from 'lodash' ;
 import './App.css';
 import logo from './jedi-logo.svg';
 const  App = ({name}) => {
-    return `<h2>welcome ${_.join(["welcome ," ,name ]," ")}</h2> <img src="${ logo }" alt="jedi logo"/> `;
+    return `<div class ="App"> <h2> Hello </h2> <h2> ${_.join(["Welcome to" ,name ]," ")}</h2> <img src="${ logo }" alt="jedi logo"/></div> `;
     
 };
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import App from "./App";
 
 const root = document.getElementById("root");
-root.innerHTML = `<h2> hello </h2> ${ App({name:"sushil"}) }`;
+root.innerHTML = ` ${ App({name:"Vanilla Js Starter"}) }`;
